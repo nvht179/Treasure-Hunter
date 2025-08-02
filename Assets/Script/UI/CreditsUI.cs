c6e9972427b563dd2c7639526122aadb082eca16
Kashi5102 <thong16112004@gmail.com>
2025-08-02 22:27:19 +0700

    feat: Add CreditScene

---
+++ Assets/Script/UI/CreditsUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsUI : MonoBehaviour
+{
+    [SerializeField] private Button backButton;
+
+    private void Awake() {
+        backButton.onClick.AddListener(() => {
+            SceneLoader.Load(SceneLoader.Scene.MainMenuScene);
+        });
+    }
+}
