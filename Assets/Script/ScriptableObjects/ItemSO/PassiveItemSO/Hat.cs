9de378362c0e6a4355d350291da4a2cb6b9761ba
zRain204 <126950997+zRain204@users.noreply.github.com>
2025-08-06 14:54:29 +0700

    feat: add Hat

---
+++ Assets/Script/ScriptableObjects/ItemSO/PassiveItemSO/Hat.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Passive Items/Hat")]
+public class Hat : PassiveItemSO
+{
+    private Guid id;
+
+    public float healthRegenerationAmount = 0.1f; // e.g., 0.1 means +10% health regeneration
+
+    private void OnValidate()
+    {
+        description = "Passive: Provides " + (healthRegenerationAmount * 100).ToString() + "% health regeneration";
+        droppable = true;
+        consumable = false;
+    }
+    public override void ApplyEffect(Player player)
+    {
+        player.HealthSystem.AddRegenBuff(healthRegenerationAmount);
+    }
+
+    public override void RemoveEffect(Player player)
+    {
+        player.HealthSystem.RemoveRegenBuff(id);
+    }
+}
